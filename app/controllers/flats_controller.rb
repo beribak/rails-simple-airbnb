a4f20773072ce2c8af3a3fc6d2846d657d6ab575
danko beribak <beribakdanko@gmail.com>
2019-08-07 23:59:54 +0200

    started destroy action in flat controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -14,6 +14,12 @@ class FlatsController < ApplicationController
     redirect_to flats_path
   end
 
+  def delete
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
   private
 
   def flat_params
