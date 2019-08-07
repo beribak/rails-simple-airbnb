d6368c736dacc0499c36a47110da76bee7e8e16a
danko beribak <beribakdanko@gmail.com>
2019-08-07 23:38:08 +0200

    create added

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,4 +3,20 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description)
+  end
 end
